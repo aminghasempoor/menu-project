ec88317faad2002f7a464c98f994936b819b2dad
AminGhasempoor <aminghasempoor79@gmail.com>
2025-01-27 10:21:43 +0330

    install packages

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import {useTranslations} from "next-intl";
+
+export default function Home() {
+  const t = useTranslations();
+  return (
+    <div>
+      <h1>{t('HomePage.title')}</h1>
+    </div>
+  );
+}
