2d9e648b64be682f8212cff47446bbdfaae7cee3
AminGhasempoor <aminghasempoor79@gmail.com>
2025-01-27 10:22:27 +0330

    formatting

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,12 +1,15 @@
-import {NextIntlClientProvider} from 'next-intl';
-import {getMessages} from 'next-intl/server';
-import {notFound} from 'next/navigation';
-import {routing} from '@/i18n/routing';
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
 import React from "react";
 
-export default async function LocaleLayout({children,params: {locale}}: {
+export default async function LocaleLayout({
+    children,
+    params: { locale },
+}: {
     children: React.ReactNode;
-    params: {locale: string};
+    params: { locale: string };
 }) {
     if (!routing.locales.includes(locale as any)) {
         notFound();
@@ -15,11 +18,9 @@ export default async function LocaleLayout({children,params: {locale}}: {
 
     return (
         <html lang={locale}>
-        <body>
-        <NextIntlClientProvider messages={messages}>
-            {children}
-        </NextIntlClientProvider>
-        </body>
+            <body>
+                <NextIntlClientProvider messages={messages}>{children}</NextIntlClientProvider>
+            </body>
         </html>
     );
 }
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import {useTranslations} from "next-intl";
+import { useTranslations } from "next-intl";
 
 export default function Home() {
-  const t = useTranslations();
-  return (
-    <div>
-      <h1>{t('HomePage.title')}</h1>
-    </div>
-  );
+    const t = useTranslations();
+    return (
+        <div>
+            <h1>{t("HomePage.title")}</h1>
+        </div>
+    );
 }
