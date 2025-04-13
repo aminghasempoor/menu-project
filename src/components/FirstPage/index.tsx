41d3fbbe68659fce09197a9febb457d3c55e18fa
AminGhasempoor <aminghasempoor79@gmail.com>
2025-04-13 09:54:59 +0330

    rows

--- src/components/FirstPage/index.tsx
+++ src/components/FirstPage/index.tsx
@@ -1,13 +1,22 @@
 import { Navbar } from "@/components/Navbar";
 import { Banner } from "@/components/Banner";
 import { Main } from "@/components/Main";
-
+import {
+    burgerItems,
+    pizzaItems,
+    recommendedBurgerItem,
+    recommendedPizzaItem,
+    recommendedSushiItem, sushiItems,
+} from "@/core/utils/foodItems";
 export function FirstPage() {
+
     return (
         <>
             <Navbar />
             <Banner />
-            <Main />
+            <Main title={"pizza"} recommendedItems={recommendedPizzaItem} foodItem={pizzaItems} />
+            <Main title={"burger"} recommendedItems={recommendedBurgerItem} foodItem={burgerItems}/>
+            <Main title={"sushi"} recommendedItems={recommendedSushiItem} foodItem={sushiItems}/>
         </>
     );
 }
--- src/components/Main/RecommendedItem.tsx
+++ src/components/Main/RecommendedItem.tsx
@@ -1,17 +1,29 @@
 import Picture from "../../../public/burger.jpg";
 import Image from "next/image";
+import { useTranslations } from "next-intl";
 
-export function RecommendedItem() {
+export function RecommendedItem({
+                                    picture,
+                                    title,
+                                    description,
+                                    price,
+                                }: {
+    picture: string;
+    title: string;
+    description: string;
+    price: string;
+}) {
+    const t = useTranslations("RecommendedItem")
     return (
         <div className={"flex items-center justify-center"}>
             <div className="relative w-full max-w-[600px] h-[160px] rounded-2xl overflow-hidden shadow-md mb-4">
-                <Image src={Picture} alt="پیتزا مارگاریتا" fill className="object-fill" />
+                <Image src={Picture} alt="pizza" fill className="object-fill" />
                 <div className="absolute inset-0 bg-gradient-to-l from-[#fce5cd]/90 via-[#fce5cd]/70 to-transparent" />
                 <div className="absolute right-4 top-4 text-right space-y-1">
-                    <p className="text-[13px] text-gray-500">آیتم پیشنهادی</p>
-                    <h2 className="text-lg font-bold flex items-center text-black gap-1">پیتزا مارگاریتا</h2>
-                    <p className="text-sm text-gray-600">سس مارینارا، پنیر، ریحان تازه</p>
-                    <p className="font-bold text-lg text-gray-800">۳۳۵,۰۰۰ تومان</p>
+                    <p className="text-[13px] text-gray-500">{t("name")}</p>
+                    <h2 className="text-lg font-bold flex items-center text-black gap-1">{title}</h2>
+                    <p className="text-sm text-gray-600">{description}</p>
+                    <p className="font-bold text-lg text-gray-800">{price}</p>
                 </div>
             </div>
         </div>
